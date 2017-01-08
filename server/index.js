23f4032ca670861bbe6b0b220786c60bee4313c2
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2017-01-08 02:52:23 -0700

    added hot reloader

--- server/index.js
+++ server/index.js
@@ -5,9 +5,17 @@ import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackConfig from '../webpack.config.dev.js';
 
+
+import webpackHotMiddleware from 'webpack-hot-middleware';
 let app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+const compiler = webpack(webpackConfig);
+app.use(webpackMiddleware(compiler, {
+  hot:true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfro: true
+}));
+app.use(webpackHotMiddleware(compiler))
 
 app.get('/*', (req,res) => {
   res.sendFile(path.join(__dirname, './index.html'));
