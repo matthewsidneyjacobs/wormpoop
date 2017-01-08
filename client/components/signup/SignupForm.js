d642283033f1f6472b77f4d124ee91d62c7ef983
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2017-01-08 03:42:30 -0700

    added signup form and some state

---
+++ client/components/signup/SignupForm.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import map from 'lodash/map';
+
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email:'',
+      password: '',
+      passwordConfirmation: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]:e.target.value});
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Join Our Community</h1>
+
+        <div className="form-group">
+          <label className="control-label">User Name</label>
+          <input
+            value={this.state.username}
+            onChange={this.onChange}
+            type="text"
+            name="username"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Email</label>
+          <input
+            value={this.state.email}
+            onChange={this.onChange}
+            type="text"
+            name="email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Password</label>
+          <input
+            value={this.state.password}
+            onChange={this.onChange}
+            type="text"
+            name="password"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Password Confirmation</label>
+          <input
+            value={this.state.passwordConfirmation}
+            onChange={this.onChange}
+            type="text"
+            name="passwordConfirmation"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <button className="btn btn-primary btn-lg">Sign Up</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SignupForm;
