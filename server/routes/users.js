306996fbd80da161146cc23f901e8a2bf9108fc1
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2017-01-08 11:55:36 -0700

    validate username on client side

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,33 +1,64 @@
 import express from 'express';
-import validateInput from '../shared/validations/signup';
+import commonValidations from '../shared/validations/signup';
 import bcrypt from 'bcrypt';
-
+import isEmpty from 'lodash/isEmpty';
 
 import User from '../models/user';
 
 let router = express.Router();
 
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data);
+
+  return User.query({
+    where: { email: data.email },
+    orWhere: { username: data.username }
+  }).fetch().then(user => {
+    if (user) {
+      if (user.get('username') === data.username) {
+        errors.username = 'Username already taken';
+      }
+      if (user.get('email') === data.email) {
+        errors.email = 'Email already taken';
+      }
+    }
+
+    return {
+      errors,
+      isValid: isEmpty(errors)
+    };
+  })
+
+}
+
+// router.get('/:identifier', (req, res) => {
+//   User.query({
+//     select: [ 'username', 'email' ],
+//     where: { email: req.params.identifier },
+//     orWhere: { username: req.params.identifier }
+//   }).fetch().then(user => {
+//     res.json({ user });
+//   });
+// });
 
 
+router.post('/', (req, res) => {
+  validateInput(req.body, commonValidations).then(({ errors, isValid }) => {
+    if (isValid) {
+      const { username, password,  email } = req.body;
+      const password_digest = bcrypt.hashSync(password, 10);
 
+      User.forge({
+        username, email, password_digest
+      }, { hasTimestamps: true }).save()
+        .then(user => res.json({ success: true }))
+        .catch(err => res.status(500).json({ error: err }));
 
-router.post('/',(req, res) => {
-  const { errors, isValid } = validateInput(req.body);
+    } else {
+      res.status(400).json(errors);
+    }
+  });
 
-  if (isValid) {
-    const { username, password, email } = req.body;
-    const password_digest = bcrypt.hashSync(password, 10);
-
-    User.forge({
-      username,email,password_digest
-    }, {hasTimestamps: true}).save()
-    .then(user => res.json({success:true}))
-    .catch(err => res.status(500).json({error:err}));
-
-    // res.json({ success:true });
-  }else {
-    res.status(400).json(errors);
-  }
 });
 
 export default router;
