fecd5c58740a5845a4ede859cfa299ff57d06005
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2017-01-08 13:34:54 -0700

    set up jwtoken for credentials

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import webpackMiddleware from 'webpack-dev-middleware';
 import webpackConfig from '../webpack.config.dev.js';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import users from './routes/users';
-
+import auth from './routes/auth';
 
 let app = express();
 app.use(bodyParser.json());
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 
 const compiler = webpack(webpackConfig);
