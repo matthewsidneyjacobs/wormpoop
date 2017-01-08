4d87b8a88ff5b2125f6a3712ee86b24c28a21063
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2017-01-08 03:18:42 -0700

    working on react router

--- client/components/Greetings.js
+++ client/components/Greetings.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-export default () => {
-  return (
-    <h1>Hi from greeting component!!</h1>
-  )
+class Greetings extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Hi from greeting component!!</h1>
+      </div>
+    );
+
+  }
 }
+
+export default Greetings;
