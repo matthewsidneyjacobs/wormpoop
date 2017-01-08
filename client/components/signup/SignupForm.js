547bbe30f12de46027d516e1e2e08fedc8dc8532
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2017-01-08 10:33:36 -0700

    add flash message reducer

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -47,6 +47,10 @@ class SignupForm extends React.Component {
     if (this.isValid()) {
       this.setState({ errors: {}, isLoading:true });
       this.props.userSignupRequest(this.state).then(() => {
+        this.props.addFlashMessage({
+          type: 'success',
+          text: 'Successful sign up'
+        })
         this.context.router.push('/');
       },
       ({ data }) => this.setState({errors:data, isLoading:false})
@@ -101,7 +105,8 @@ class SignupForm extends React.Component {
 }
 
 SignupForm.propTypes = {
-  userSignupRequest: React.PropTypes.func.isRequired
+  userSignupRequest: React.PropTypes.func.isRequired,
+  addFlashMessage: React.PropTypes.func.isRequired
 }
 SignupForm.contextTypes = {
   router: React.PropTypes.object.isRequired
--- client/index.js
+++ client/index.js
@@ -3,17 +3,21 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './rootReducer';
 
 import routes from './routes';
 
 
 const store = createStore(
-  (state = {}) => state,
-  applyMiddleware(thunk)
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
 );
 
 render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
-  </Provider>, document.getElementById('app')); 
+  </Provider>, document.getElementById('app'));
